f73e3f5285b9785a5c809599747f5b1b8ccb4044
lenkly <lenk.lydia@gmail.com>
2020-02-26 14:29:31 +0100

    Add Clear and Result functions

--- src/calculator.js
+++ src/calculator.js
@@ -1,1 +1,33 @@
-document.querySelector(".calc__output").value = "Let's calculate!";
+"use strict";
+
+const calcOutput = document.querySelector(".calc__output");
+const calcInputs = document.querySelectorAll(".calc__button--num"); //holt sich alle Zahlen
+const calcResult = document.querySelector(".calc__button--res");
+const calcClear = document.querySelector(".calc__button--clear");
+
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+}
+function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
+function divide(numberOne, numberTwo) {
+  return numberOne / numberTwo;
+}
+
+const numberOne = Number(calcInputs[5].innerText);
+const numberTwo = Number(calcInputs[8].innerText);
+
+function handleClick() {
+  calcOutput.value = add(numberOne, numberTwo);
+  console.log("clicked");
+}
+calcResult.addEventListener("click", handleClick);
+
+function clear() {
+  calcOutput.value = "";
+}
+calcClear.addEventListener("click", clear);
